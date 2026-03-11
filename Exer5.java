f610e2fdc0939aa47d540b5a71b7f6603d68fafb
Rachel Coelho <163034541+RachelCoelho@users.noreply.github.com>
2026-03-11 09:05:18 -0300

    Update Exer5.java

--- Exer5.java
+++ Exer5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Salario com percentual de aumento {
+public class Salario {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
