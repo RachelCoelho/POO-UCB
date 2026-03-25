316a2a822331608e83213fd82e8714515e2173dc
Rachel Coelho <163034541+RachelCoelho@users.noreply.github.com>
2026-03-24 22:48:36 -0300

    Update Produto.java

--- ControleDeEstoque/Produto.java
+++ ControleDeEstoque/Produto.java
@@ -1,1 +1,30 @@
+public class Produto {
 
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidade;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+        System.out.println("Quantidade: " + quantidadeEmEstoque);
+    }
+
+    public void adicionarAoEstoque(int quantidade) {
+        quantidadeEmEstoque += quantidade;
+    }
+
+    public void removerDoEstoque(int quantidade) {
+        if (quantidade <= quantidadeEmEstoque) {
+            quantidadeEmEstoque -= quantidade;
+        } else {
+            System.out.println("Estoque insuficiente!");
+        }
+    }
+}
