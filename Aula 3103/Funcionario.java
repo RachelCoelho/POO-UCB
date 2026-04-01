0dbc506187d9b85af1be489ffcc26c8516c228e3
Rachel Coelho <163034541+RachelCoelho@users.noreply.github.com>
2026-03-31 21:13:39 -0300

    Update and rename Funcionario to Funcionario.java

---
+++ Aula 3103/Funcionario.java
@@ -0,0 +1,17 @@
+package com.mycompany.poo;
+
+
+public class Funcionario extends Pessoa {
+    private double salario;
+    
+    public Funcionario(String nome, int idade, double salario){
+        super(nome, idade);
+        this.salario = salario;
+    }
+    
+    @Override
+    public void mostrarDados(){
+        super.mostrarDados();
+        System.out.println(" Salario: " + salario);
+    }
+}
