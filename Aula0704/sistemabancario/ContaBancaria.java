763d864e621b4541efa14b0b12b7aceea3926419
Rachel Coelho <163034541+RachelCoelho@users.noreply.github.com>
2026-04-08 09:42:10 -0300

    Update ContaBancaria.java

--- Aula0704/sistemabancario/ContaBancaria.java
+++ Aula0704/sistemabancario/ContaBancaria.java
@@ -1,1 +1,29 @@
+package com.mycompany.sistemabancario;
 
+public class ContaBancaria {
+
+    protected String titular;
+    protected double saldo;
+
+    public ContaBancaria(String titular, double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
+    public void exibirDados() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: " + saldo);
+    }
+}
